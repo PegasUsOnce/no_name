bd5ffa0d39cd5838f2f77a2e54fa0c8e248cbfe3
PegasUsOnce <rosliakovaov@gmail.com>
2022-09-01 18:51:04 +0300

    я проверила capacity stringbuilder

---
+++ Tests/UnitTests/StringTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestFixture, NonParallelizable]
+    internal class StringTests
+    {
+        [Test]
+        public void StringBuilderCapacityTest()
+        {
+            var builder = new StringBuilder();
+            
+            Assert.AreEqual(16, builder.Capacity);
+
+            for (int i = 1; i < 19; i++)
+            {
+                var stringToAppend = $"some string number {i % 10}\n";
+                builder.Append(stringToAppend);
+                var builderCapacity = builder.Capacity;
+
+                var expectedCapacity = Math.Pow(2, Math.Ceiling(Math.Log2(stringToAppend.Length * i)));
+
+                Assert.AreEqual(expectedCapacity, builderCapacity);
+            }
+        }
+    }
+}
